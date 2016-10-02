d413d9e7a4182865435f25b62bf5b786e669f354
Ivan <ivanbashkirov2@tutanota.com>
2016-10-02 03:20:22 +0300

    cleaned up

--- scripts/landing.js
+++ scripts/landing.js
@@ -10,7 +10,6 @@ var animatePoints = function() {
         points[i].style.msTransform = "scaleX(1) translateY(0)";            points[i].style.WebkitTransform = "scaleX(1) translateY(0)";    
     };
     
-    console.log(points.length);
     for (var i = 0; i < points.length; i++) {
         
         revealPoint(i);
